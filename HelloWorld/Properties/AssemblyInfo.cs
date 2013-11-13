06695dc0725bf1c4e18ec10c25d0e02e9145c71d
DevOps Management <devopsmgmt@gmail.com>
2013-11-13 19:23:13 +0530

    Hello World

--- HelloWorld/Properties/AssemblyInfo.cs
+++ HelloWorld/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("c36468c0-d49f-44b4-9ec2-de227644bc69")]
+[assembly: Guid("d1e325cd-f337-4842-a224-c6d8514a980f")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,7 +29,8 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Revision and Build Numbers 
+// You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
