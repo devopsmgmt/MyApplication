2fe9202b017e60d5947d59079e26b714bbdacd8c
DevOps Management <devopsmgmt@gmail.com>
2013-11-13 19:25:39 +0530

    Hello World MVC4

--- HelloWorld/Properties/AssemblyInfo.cs
+++ HelloWorld/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("d1e325cd-f337-4842-a224-c6d8514a980f")]
+[assembly: Guid("9959daae-ec2e-426e-966e-fc3f166b2f1b")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,8 +29,7 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
+// You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
