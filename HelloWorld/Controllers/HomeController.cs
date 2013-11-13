581c33b3a8396b764d7b3700e61b0a1b2b01451a
DevOps Management <devopsmgmt@gmail.com>
2013-11-13 15:51:26 +0530

    Made a small change

--- HelloWorld/Controllers/HomeController.cs
+++ HelloWorld/Controllers/HomeController.cs
@@ -24,7 +24,7 @@ namespace HelloWorld.Controllers
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "Your contact page";
 
             return View();
         }
